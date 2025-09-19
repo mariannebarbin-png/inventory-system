d62854112864f15bf03873b7da658d520e9878a7
bangalaochiquecruiz-dot <bangalaochiquecruiz@gmail.com>
2025-09-19 15:57:54 +0800

    Update Item.php

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -10,8 +10,8 @@ class Item extends Model
         use HasFactory;
 
         protected $fillable =[
-        'name',
-        'quantity',
-        'price',
+            'name',
+            'quantity',
+            'price',
         ];
     }
