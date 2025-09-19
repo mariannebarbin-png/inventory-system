f85ebfce05a805ce2223ebf3c9d95cec77ecc731
bangalaochiquecruiz-dot <bangalaochiquecruiz@gmail.com>
2025-09-19 15:54:45 +0800

    Update Transaction.php

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -1,1 +1,17 @@
-//butangi
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactpry;
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction extends Model
+    {
+        use HasFactory;
+
+        protected $fillable =[
+            'user_id',
+            'item_id',
+            'quantity',
+        ];
+    }
